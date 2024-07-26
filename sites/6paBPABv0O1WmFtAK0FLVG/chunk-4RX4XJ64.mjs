e8ac4ea280a0bfb2202c764ec170547eb8ce0b09
Your Name <you@example.com>
2024-07-26 17:28:22 +0800

    Initial commit

---
+++ sites/6paBPABv0O1WmFtAK0FLVG/chunk-4RX4XJ64.mjs
@@ -0,0 +1,2 @@
+function a(t,e){return{description:`Say hello to Mr.Wow, who started trending on TikTok, Instagram, and YouTube, now he's gonna make some "WOW" on Solana!`,favicon:"https://framerusercontent.com/assets/OUTQ1hxKLlneNqlpMcCgqzX3gM.jpg",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/OUTQ1hxKLlneNqlpMcCgqzX3gM.jpg",title:"$WOWCat"}}export{a};
+//# sourceMappingURL=chunk-4RX4XJ64.mjs.map
